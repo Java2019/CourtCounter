3b297904839a01a775ec79ae03dac630c69933a5
java2019 <java_2019@mail.ru>
2017-07-01 12:55:08 +0300

    Initial commit

---
+++ app/src/main/java/courtcounter/courtcounter/CourtCounterActivity.java
@@ -0,0 +1,13 @@
+package courtcounter.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CourtCounterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_court_counter);
+    }
+}
